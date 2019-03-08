39bdc099abf85809bf8f81b9cf66e91838e91750
kipkorir cornelius <mishrakiprop@gmail.com>
2019-03-08 06:39:40 +0300

    add the user interface logic

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// UI logic.
+
+$(document).ready(function () {
+
+    var routes = ["juja", "mombasa", "waiyaki", "jogoo", "ngong", "kiambu", "thika"];
+
+        routes[0]= ["Eastleigh", "Jonsaga", "Kariobangi", "Babadogo"];
+        routes[1]= ["Rasasi", "City hoppa", "Indima nje", "Nyayo" ];
+        routes[2]= ["Star Bus", "Super Metro", "Metro", "Latema"];
+        routes[3]= ["Umoinner", "Embasava", "Donni", "Pipepline" ];
+        routes[4]= ["Rukangu", "Citi Hoppa", "KBS", "Nangiks"];
+        routes[5]= ["Banana", "Ruaka", "BHM", "Lopha Travels"];
+        routes[6]= ["Nakaski", "Kenya Mpya", "Manchester", "Rukagina"];
+        $("#Juja").click(function(){
+          $("#show").text(routes[0])
+        
+        })
