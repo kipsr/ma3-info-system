58b6eb107f888538729f01006dab1f21f793c4b6
kipkorir cornelius <mishrakiprop@gmail.com>
2019-03-08 06:43:42 +0300

    add subtmit button

--- js/scripts.js
+++ js/scripts.js
@@ -39,4 +39,18 @@ $(document).ready(function () {
             $("#show").text(routes[6])
           
           })
+
+          ("#submit").click(function(){
+            $("#submit").text("Your pickup point is along Ronald Ngala street.Travel safe!")
+          })
+  
+          $("#button").submit(function(event){
+          var nameInput =$("input#name").val();
+          var routeInput = $("input#route").val();
+          var idInput = $("input#Id").val();
+      
+          $(".name1").text(nameInput);
+          $(".route1").text(routeInput);
+          $(".id no1").text(idInput);
+      
   
